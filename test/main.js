d044813618ab84df726439d0f36f2b9b46bcb85a
pindab0ter <hansvanluttikhuizen@me.com>
2016-02-09 17:14:49 +0100

    Set up testing framework

--- test/main.js
+++ test/main.js
@@ -1,1 +1,25 @@
-// TODO Create tests
+'use strict';
+
+var assert = require('assert'),
+    context = require('mocha').describe,
+    it = require('mocha').it,
+    gulp = require('gulp'),
+    filenamelist = require('../'),
+    source = ['source/*'],
+    destination = 'destination/';
+
+context('without any options specified', function() {
+
+    it('should show the default behaviour', function(done) {
+        var stream = gulp.src(source)
+            .pipe(filenamelist())
+            .pipe(gulp.dest(destination));
+
+        stream.on('end', function() {
+            // TODO: Verify contents
+            done();
+        });
+
+    });
+
+});
